c6e812899aa57c7215e183a9b488ca666e7dd792
Grayson <info@fromconcentratesoftware.com>
2016-05-05 21:37:37 -0400

    Initial Commit

---
+++ Using/Using.h
@@ -0,0 +1,19 @@
+//
+//  Using.h
+//  Using
+//
+//  Created by Grayson Hansard on 5/5/16.
+//  Copyright © 2016 From Concentrate Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Using.
+FOUNDATION_EXPORT double UsingVersionNumber;
+
+//! Project version string for Using.
+FOUNDATION_EXPORT const unsigned char UsingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Using/PublicHeader.h>
+
+
